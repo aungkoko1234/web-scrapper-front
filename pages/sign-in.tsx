5d3faa63349c9d56a51f9f394dadd96a88a09c87
aungkoko1234 <aungkokouit2096@gmail.com>
2023-01-01 22:51:37 +0630

    feat - implement auth page

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,8 +2,8 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -12,6 +12,15 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import styles from "../styles/Home.module.scss";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { apiClient } from "../lib/httpClient";
+import TextInputComponent from "../components/control/TextInputComponent";
+import { Snackbar, Stack } from "@mui/material";
+import { Action } from "../components/SnackbarAction";
+import { SignInResponseDto } from "../lib/interface";
+import { setAuthState } from "../store/authSlice";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/router";
 
 // function Copyright(props: any) {
 //   return (
@@ -33,14 +42,52 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
 
+interface SignInDto {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [message, setMessage] = useState<string>("");
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email().required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (data: SignInDto) => {
+    setLoading(true);
+    const url = "/auth/sign-in";
+    const body = {
+      email: data.email,
+      password: data.password,
+    };
+    apiClient(process.env.NEXT_PUBLIC_API_URL)
+      .post(url, { ...body })
+      .then((data) => {
+        const response = data.data?.data as SignInResponseDto;
+        console.log("response", response);
+        dispatch(
+          setAuthState({
+            isAuthenticated: true,
+            accessToken: response.accessToken,
+            email: response.profile.email,
+            userName: response.profile.userName,
+          })
+        );
+        setLoading(false);
+        router.push("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        setShowMessage(true);
+        setMessage(error.response.data.message || "");
+      });
   };
 
   return (
@@ -62,58 +109,68 @@ export default function SignIn() {
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+            <Box component="div" sx={{ mt: 3, width: "100%" }}>
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  phone: "",
+                  type: "",
+                  message: "",
+                }}
+                validationSchema={validationSchema}
+                enableReinitialize={true}
+                onSubmit={(values, { resetForm }) => {
+                  handleSubmit(values as unknown as SignInDto);
+                  resetForm();
+                }}
               >
-                Sign In
-              </Button>
-              {/* <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid> */}
+                <Form>
+                  <Grid container>
+                    <Grid item md={12} xs={12}>
+                      <Stack
+                        direction="column"
+                        spacing={2}
+                        my={2}
+                        justifyContent="flex-start"
+                      >
+                        <TextInputComponent
+                          label="Email Address"
+                          placeholder="Enter Email"
+                          name="email"
+                        />
+                        <TextInputComponent
+                          name="password"
+                          placeholder="Enter Password"
+                          label="Password"
+                          type="password"
+                          autoComplete="new-password"
+                        />
+                        <Button
+                          variant="contained"
+                          type="submit"
+                          fullWidth
+                          disabled={isLoading}
+                          sx={{ mt: 3, mb: 2 }}
+                        >
+                          Sign In
+                        </Button>
+                      </Stack>
+                    </Grid>
+                  </Grid>
+                </Form>
+              </Formik>
             </Box>
           </Box>
+          <Snackbar
+            color="error"
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            autoHideDuration={5000}
+            action={<Action handleClose={() => setShowMessage(false)} />}
+            open={showMessage}
+            onClose={() => setShowMessage(false)}
+            message={message}
+          />
         </Container>
       </main>
     </ThemeProvider>
--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,9 +1,8 @@
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -12,17 +11,43 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import styles from "../styles/Home.module.scss";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { apiClient } from "../lib/httpClient";
+import TextInputComponent from "../components/control/TextInputComponent";
+import { Stack } from "@mui/system";
+import { Snackbar } from "@mui/material";
+import { Action } from "../components/SnackbarAction";
 
 const theme = createTheme();
+interface SignupDto {
+  userName: string;
+  email: string;
+  password: string;
+}
 
 export default function SignUp() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+  const validationSchema = Yup.object().shape({
+    userName: Yup.string().required("Name is required"),
+    email: Yup.string().email().required("Email is required"),
+    password: Yup.string().min(8, "Password must be at least 8 characters"),
+  });
+
+  const handleSubmit = (data: SignupDto) => {
+    setLoading(true);
+    const url = "/auth/sign-up";
+    const body = {
+      userName: data.userName,
+      email: data.email,
+      password: data.password,
+    };
+    apiClient(process.env.NEXT_PUBLIC_API_URL)
+      .post(url, { ...body })
+      .then((data) => {
+        setLoading(false);
+        setShowMessage(true);
+      });
   };
 
   return (
@@ -44,81 +69,80 @@ export default function SignUp() {
             <Typography component="h1" variant="h5">
               Sign up
             </Typography>
-            <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
-            >
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    autoComplete="given-name"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="family-name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox value="allowExtraEmails" color="primary" />
-                    }
-                    label="I want to receive inspiration, marketing promotions and updates via email."
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+            <Box component="div" sx={{ mt: 3, width: "100%" }}>
+              <Formik
+                initialValues={{
+                  name: "",
+                  email: "",
+                  phone: "",
+                  type: "",
+                  message: "",
+                }}
+                validationSchema={validationSchema}
+                enableReinitialize={true}
+                onSubmit={(values, { resetForm }) => {
+                  handleSubmit(values as unknown as SignupDto);
+                  resetForm();
+                }}
               >
-                Sign Up
-              </Button>
+                <Form>
+                  <Grid container>
+                    <Grid item md={12} xs={12}>
+                      <Stack
+                        direction="column"
+                        spacing={2}
+                        my={2}
+                        justifyContent="flex-start"
+                      >
+                        <TextInputComponent
+                          name="userName"
+                          placeholder="Enter User Name"
+                          label="User Name"
+                        />
+                        <TextInputComponent
+                          label="Email Address"
+                          placeholder="Enter Email"
+                          name="email"
+                        />
+                        <TextInputComponent
+                          name="password"
+                          placeholder="Enter Password"
+                          label="Password"
+                          type="password"
+                          autoComplete="new-password"
+                        />
+                        <Button
+                          variant="contained"
+                          type="submit"
+                          fullWidth
+                          disabled={isLoading}
+                          sx={{ mt: 3, mb: 2 }}
+                        >
+                          Sign Up
+                        </Button>
+                      </Stack>
+                    </Grid>
+                  </Grid>
+                </Form>
+              </Formik>
+
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/sign-in" variant="body2">
                     Already have an account? Sign in
                   </Link>
                 </Grid>
               </Grid>
             </Box>
           </Box>
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            autoHideDuration={5000}
+            action={<Action handleClose={() => setShowMessage(false)} />}
+            open={showMessage}
+            onClose={() => setShowMessage(false)}
+            message={"Sign Up is successfully processed"}
+          />
         </Container>
       </main>
     </ThemeProvider>
